8ad3c9223e726d9faa9b564fc2ecfaffb05c2f87
varun08chaubey <varun08chaubey@gmail.com>
2023-12-27 18:17:21 +0530

    initial setup there are 5 lines

---
+++ GitDemo1/src/tests/GitTest1.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		System.out.println("This is line one ");
+		System.out.println("This is line two ");
+		System.out.println("This is line three ");
+		System.out.println("This is line four ");
+		System.out.println("This is line five ");
+		
+	}
+
+}
